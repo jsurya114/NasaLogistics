2e92fb6832110421ba167785fc48217b3d991e3e
Yadhu Krishna <yadhu808krishna@gmail.com>
2025-09-10 20:37:53 +0530

    feat/folder structure

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import adminRoutes from './routes/adminRoutes.js'
 
 const app = express();
 const PORT:number = Number(process.env.PORT);
@@ -10,6 +11,9 @@ const PORT:number = Number(process.env.PORT);
 app.get('/', (req: Request, res: Response) => {
   res.send('Backend running!');
 });
+
+app.use('/admin',adminRoutes);
+app.use('/user',)
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
 
---
+++ Backend/src/routes/adminRoutes.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+const router=express.Router();
+// import adminController from './../controllers/adminController.js'
+
+// router.post('/admin/upload-data',)
+
+
+
+export default router;
+
